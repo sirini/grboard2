43c9d5f22cfa63f02313868e16b066dbb0595498
sirini <sirini@gmail.com>
2015-07-19 12:53:09 +0900

    검색 시 특수문자도 검색이 되도록 수정함 (Thanks to ViTZrO)  - 다만, - (하이픈) 과 ; (세미콜론) 은 제외 (SQL 쿼리문으로 이용할 수 있으므로)

--- module/board/search.php
+++ module/board/search.php
@@ -1,6 +1,6 @@
 <?php
 if(!defined('GR_BOARD_2')) exit();
 if(array_key_exists('option', $_GET)) $searchOption = $Common->getPlaneText($_GET['option']);
-if(array_key_exists('value', $_GET)) $searchValue = $Common->getPlaneText($_GET['value']);
+if(array_key_exists('value', $_GET)) $searchValue = str_replace(array('-', ';'), '', $_GET['value']);
 include 'list.php';
 ?>
